6553bc6477c59b45084b39d67dd590f37c23f38c
Dave Bullough <dave.bullough@gmail.com>
2015-07-13 16:31:33 +0100

    Added test for mocking server

--- index.js
+++ index.js
@@ -1,48 +1,109 @@
 /**
  * [description]
- * @param  {[type]} ) {	var        names [description]
+ * @param  {[type]} ) { var        names [description]
  * @return {[type]}   [description]
  */
 var randomWorldFactory = (function () {
 
-	var signature  	= {},
-		_ 			= require('underscore'), 
-		fs 			= require('fs'), 
-		path 		= require('path'),
-		libFilter 	= '.js', 
-		libPath 	= './lib',
-		libraries 	= fs.readdirSync(path.normalize(libPath));
-	
+    var signature   = {},
+        _           = require('underscore'), 
+        fs          = require('fs'), 
+        path        = require('path'),
+        libFilter   = '.js', 
+        libPath     = './lib',
+        libraries   = fs.readdirSync(path.normalize(libPath));
+    
 
-	/**
-	 * collate the libs and add them to the factory output
-	 * 
-	 * @param  {[type]} lib){		if (path.extname(lib) [description]
-	 * @return {[type]}            [description]
-	 */
-	libraries.forEach(function(lib){
+    signature.fromMock = function () {
 
-		if (path.extname(lib) === libFilter) {
-			register(lib);
-		}
-	});
+        var mock,
+            string = { 
+            "type": "collection",
+            "struct": {
+                "title":  "$sentence",
+                "title1":  "$fullName",
+                "id": "$integer",
+                "firsname": "$foreName",
+                "name": "Hello my name is $foreName{daveEnabled:1} and I am $integer years old, also this: $title{foo: 1}."
+            }
+        };
 
-	/**
-	 * registers the methods in the library in the sig of this factory
-	 * 
-	 * @param  {[type]} lib [description]
-	 * @return {[type]}     [description]
-	 */
-	function register(lib) {
 
-		var moduleName 	= path.basename(lib, libFilter),
-			instance 	= require(path.resolve(libPath, lib)); 
+        tokenize = function (str) {
 
-		_.extend(signature, instance);
-	}
+            var re = /\$([a-zA-Z0-9]+)(\{([a-zA-Z0-9\:\ ]+)\})?/g,
+                tokens = str.match(re);
 
-	return signature;
-	
+            _.each(tokens, function(match){
+
+                var key = match, 
+                    options = key.match(/\{([a-zA-Z0-9\:\ ]+)\}/);
+
+                if (options) {
+                    console.log(match, options);
+                }       
+                //console.log(match, options);
+            });
+            
+            
+            console.log('-----------------------')
+
+            return str;
+        };
+
+
+        switch (string.type) {
+            case 'collection':
+                var struct = string.struct;
+                    
+                mock = [];
+
+                for (var i = 0; i < 1; i++) {
+                    data = {};
+                    _.keys(struct).forEach(function(key) {
+                        data[key] = tokenize(struct[key]);
+                    });
+                    mock.push(data);
+                }
+                break;
+        }
+
+        // console.log(mock);
+
+        return mock;
+    };
+
+    signature.fromMock();
+
+    /**
+     * collate the libs and add them to the factory output
+     * 
+     * @param  {[type]} lib){       if (path.extname(lib) [description]
+     * @return {[type]}            [description]
+     */
+    libraries.forEach(function(lib){
+
+        if (path.extname(lib) === libFilter) {
+            register(lib);
+        }
+    });
+
+    /**
+     * registers the methods in the library in the sig of this factory
+     * 
+     * @param  {[type]} lib [description]
+     * @return {[type]}     [description]
+     */
+    function register(lib) {
+
+        var moduleName  = path.basename(lib, libFilter),
+            instance    = require(path.resolve(libPath, lib)); 
+
+        _.extend(signature, instance);
+    }
+
+    return signature;
+
 })();
 
 module.exports = randomWorldFactory;
---
+++ tests/mock-server.tests.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect,
+	random = require('../index');
+
+/**
+ * name tests
+ * 
+ * @return {[type]}   [description]
+ */
+describe('Mocking server tests:', function() {
+	describe('fromMock()', function () {
+		it('should return a number', function () {
+			expect(random.fromMock()).to.be.a('string');
+		});
+	});
+});
