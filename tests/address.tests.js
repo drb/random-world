152c936469e747ab7d8794058a7a0cb58c861575
Dave Bullough <dave.bullough@gmail.com>
2015-07-30 14:51:33 +0100

    Added support to create a random city and country

---
+++ tests/address.tests.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect,
+    random = require('../index');
+
+/**
+ * Address tests
+ * 
+ * @return {[type]}   [description]
+ */
+describe('Address tests:', function() {
+
+    describe('Address methods', function () {
+
+        it('city() should return a city name', function () {
+            var city = random.city();
+            expect(city)
+                .to.be.a('string')
+                .to.have.length.above(0);
+        });
+
+        it('city() should return a city name within the UK', function () {
+            var city = random.city({country: 'United Kingdom'});
+            expect(city)
+                .to.be.a('string')
+                .to.have.length.above(0);
+        });
+
+        it('should return a country', function () {
+            var country = random.country();
+            expect(country)
+                .to.be.a('string')
+                .to.have.length.above(0);
+        });
+    });
+});
