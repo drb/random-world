a35a019c347fc88d8fb6593e83dee211f2a3ac8a
Dave Bullough <dave.bullough@gmail.com>
2015-10-03 20:22:16 +0100

    Added credit card methods with tests. Fixed some of the dictionary data.

--- tests/money.tests.js
+++ tests/money.tests.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect,
-    random = require('../index');
+    random = require('../index'),
+    utils  = require('../lib/utils/utilities');
 
 /**
  * Money tests
@@ -10,20 +11,97 @@ describe('Money tests:', function() {
 
     describe('Money methods', function () {
 
-        it('ccnumber() should return a credit card number', function () {
+        it('ccnumber() should return a credit card number (unvalidated)', function () {
             var ccnumber = random.ccnumber();
-            console.log('ccnumber', ccnumber);
             expect(ccnumber)
                 .to.be.a('string')
                 .to.have.length.above(0);
         });
 
-        it('ccnumber() should return a credit card number separated by hypens', function () {
-            var ccnumber = random.ccnumber({hasHyphens: false});
-            console.log('ccnumber', ccnumber);
+        it('ccnumber() should return a credit card number (unvalidated), separated by hypens every 4 digits', function () {
+            var ccnumber = random.ccnumber({
+                hasHyphens: false
+            });
             expect(ccnumber)
                 .to.be.a('string')
+                .to.have.length.above(0)
+                .to.match(/^(\d+-?)+\d+$/);
+        });
+        
+        it('ccnumber() should create 1000 CC numbers and validate them all against utils\' MOD 10 check', function () {
+            expect(function() {
+                var ccnumber;
+                for (var i = 0; i < 1000; i++) {
+                    ccnumber = random.ccnumber();
+                    if (!utils.luhnModCheck(ccnumber.toString())) {
+                        throw new Error('Failed checksum check on ' + ccnumber);
+                    }
+                }
+            }).to.not.throw(Error);
+        });
+
+
+        it('ccnumber() should return a MasterCard credit card number that passes strict checking', function () {
+            expect(function() {
+                var ccnumber = random.ccnumber({
+                    shortName: 'MC'
+                }),
+                mcStartsWith = [51, 52, 53, 54, 55], // must start with one of these strings
+                mcMatchesOne = false;
+
+                // ensure one of the starting numbers matches
+                mcStartsWith.forEach(function(starts) {
+                    if (ccnumber.substr(0, 2) == starts) {
+                        mcMatchesOne = true;
+                    }
+                });
+
+                // error out, string doesn't match an allowed starting string
+                if (!mcMatchesOne) {
+                    throw new Error('Number does not start with allowed series.');
+                }
+
+                // error out, string doesn't match allowed length
+                if (ccnumber.length != 16 && ccnumber.length != 19) {
+                    throw new Error('Number does not start with allowed series.');   
+                }
+
+                // error out, cc number fails mod check
+                if (!utils.luhnModCheck(ccnumber.toString())) {
+                    throw new Error('Failed checksum check');
+                }
+            }).to.not.throw(Error);
+        });
+
+        it('cctype() should return a credit card type', function () {
+            var ccType = random.cctype();
+            expect(ccType)
+                .to.be.a('string')
                 .to.have.length.above(0);
+        });
+
+        it('ccstart() should return a credit card start date', function () {
+            var ccStartDate = random.ccstart();
+            expect(ccStartDate)
+                .to.be.a('string')
+                .to.have.length.above(0)
+                .to.match(/^([0-9\/]+)$/);
+        });
+
+        it('ccenddate() should return a credit card expiry date', function () {
+            var ccEndDate = random.ccexpiry();
+            expect(ccEndDate)
+                .to.be.a('string')
+                .to.have.length.above(0)
+                .to.match(/^([0-9\/]+)$/);
+        });
+
+        it('cvv() should return a credit card CV2 value', function () {
+            var cv2 = random.cvv();
+            expect(cv2)
+                .to.be.a('string')
+                .to.have.length(3)
+                .to.match(/[0-9]+$/);
         });
     });
 });
--- tests/util.tests.js
+++ tests/util.tests.js
@@ -19,4 +19,57 @@ describe('Utilities method - randomisation tests:', function() {
         });
     });
 
+    /**
+     * [description]
+     * 
+     * @return {[type]}   [description]
+     */
+    describe('utils.luhnModCheck() all valid', function () {
+        it('checks 10 known to be valid cc numbers. should not raise an exception', function () {
+            var cards = [
+                '4716300100405316',
+                '4048089364723931',
+                '4539786958616217',
+                '6011077929621219',
+                '6011687402838468',
+                '6011295950091934',
+                '5496365969435178',
+                '5588806904043709',
+                '379320072806131',
+                '0604604879605953',
+                '4844078890375122'
+            ];
+            expect(function() {
+                for (var i = 0; i < cards.length; i++) {
+                    if (!utils.luhnModCheck(cards[i])) {
+                        throw Error('Failed modulo 10 check');
+                    }
+                }
+            }).to.not.throw(Error);
+        });
+    });
+
+
+    /**
+     * [description]
+     * 
+     * @return {[type]}   [description]
+     */
+    describe('utils.luhnModCheck() with known invalid', function () {
+        it('checks 2 cc numbers, the 2nd is invalid. should raise an exception', function () {
+            var cards = [
+                '0604604879605953', // good one
+                '0604604879605952'  // bad one
+            ];
+            expect(function() {
+                for (var i = 0; i < cards.length; i++) {
+                    if (!utils.luhnModCheck(cards[i])) {
+                        throw Error('Failed modulo 10 check');
+                    }
+                }
+            }).to.throw(Error);
+        });
+    });
+
+
 });
