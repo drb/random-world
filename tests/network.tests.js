a5ef13fcc21a0c0f5ed3659559b84dc27c514abe
Dave Bullough <dave.bullough@gmail.com>
2016-02-15 11:32:24 +0000

    Fixed typo in common comment

--- tests/network.tests.js
+++ tests/network.tests.js
@@ -10,34 +10,34 @@ describe('Network tests:', function() {
 
     console.log(random.methods.network.domain());
 
-    // describe('domain()', function () {
-    //     it('should return a fully qualified domain', function () {
-    //         var domain = random.methods.network.domain();
-    //         console.log('DOMAIN', domain);
-    //         expect(domain).to.be.a('string');
-    //     });
-    // });
-    //
-    // describe('tld()', function () {
-    //     it('should return a top level domain', function () {
-    //         expect(random.tld()).to.be.a('string');
-    //     });
-    // });
-    //
-    // describe('ip()', function () {
-    //     it('should return a random ip', function () {
-    //         var ip = random.ip();
-    //         expect(ip).to.be.a('string');
-    //     });
-    // });
-    //
-    // describe('ip()', function () {
-    //     it('should return a random ip with a mask', function () {
-    //         var ip = random.ip({mask: true});
-    //         expect(ip)
-    //             .to.be.a('string')
-    //             .to.have.string('/');
-    //     });
-    // });
+    describe('domain()', function () {
+        it('should return a fully qualified domain', function () {
+            var domain = random.network.domain();
+            // console.log('DOMAIN', domain);
+            expect(domain).to.be.a('string');
+        });
+    });
+
+    describe('tld()', function () {
+        it('should return a top level domain', function () {
+            expect(random.tld()).to.be.a('string');
+        });
+    });
+
+    describe('ip()', function () {
+        it('should return a random ip', function () {
+            var ip = random.ip();
+            expect(ip).to.be.a('string');
+        });
+    });
+
+    describe('ip()', function () {
+        it('should return a random ip with a mask', function () {
+            var ip = random.ip({mask: true});
+            expect(ip)
+                .to.be.a('string')
+                .to.have.string('/');
+        });
+    });
 
 });
